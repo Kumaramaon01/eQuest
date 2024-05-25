842f4cfd2e86a315a44d063c01e990348f18e568
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-25 09:51:59 +0530

    Update sim_parserv01.py

--- SIM_Parser/sim_parserv01.py
+++ SIM_Parser/sim_parserv01.py
@@ -1,12 +1,11 @@
 import os
 import streamlit as st
-import pandas as pd
+import tempfile
 from SIM_Parser.src_sim import lv_b, ls_c, lv_d, pv_a, sv_a, beps, bepu, lvd_summary, sva_zone, ps_e, ps_f
-
 
 def get_report_and_save(report_function, name1, sim_path, file_suffix):
     report = report_function(sim_path)
-    # Get the parent directory of the INP file
+    # Get the parent directory of the SIM file
     parent_directory = os.path.dirname(sim_path)
     file_name = os.path.splitext(os.path.basename(sim_path))[0]
     file_path = os.path.join(parent_directory, f'{file_name}_{file_suffix}.csv')
@@ -18,23 +17,24 @@ def get_report_and_save(report_function, name1, sim_path, file_suffix):
 def main(uploaded_file):
     if uploaded_file is not None:
         # Save the uploaded file temporarily
-        sim_path = os.path.join(os.path.expanduser("~"), "Downloads", uploaded_file.name)
-        with open(sim_path, "wb") as f:
-            f.write(uploaded_file.getbuffer())
-            sim_content = uploaded_file.getvalue()
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(uploaded_file.getbuffer())
+            temp_file_path = temp_file.name
 
-            get_report_and_save(ls_c.get_LSC_report, sim_content, sim_path, 'lsc')
-            get_report_and_save(lv_d.get_LVD_report, sim_content, sim_path, 'lvd')
-            get_report_and_save(lvd_summary.get_LVD_Summary_report, sim_content, sim_path, 'lvd_Summary')
-            get_report_and_save(pv_a.get_PVA_report, sim_content, sim_path, 'pva')
-            get_report_and_save(sv_a.get_SVA_report, sim_content, sim_path, 'sva')
-            get_report_and_save(sva_zone.get_SVA_Zone_report, sim_content, sim_path, 'sva_Zone')
-            get_report_and_save(beps.get_BEPS_report, sim_content, sim_path, 'beps')
-            get_report_and_save(bepu.get_BEPU_report, sim_content, sim_path, 'bepu')
-            get_report_and_save(lv_b.get_LVB_report, sim_content, sim_path, 'lvb')
-            get_report_and_save(ps_e.get_PSE_report, sim_content, sim_path, 'pse')
-            get_report_and_save(ps_f.get_PSF_report, sim_content, sim_path, 'psf')
-            st.success("SIM Parsed Successfully!!")
+        sim_path = temp_file_path
 
+        get_report_and_save(ls_c.get_LSC_report, None, sim_path, 'lsc')
+        get_report_and_save(lv_d.get_LVD_report, None, sim_path, 'lvd')
+        get_report_and_save(lvd_summary.get_LVD_Summary_report, None, sim_path, 'lvd_Summary')
+        get_report_and_save(pv_a.get_PVA_report, None, sim_path, 'pva')
+        get_report_and_save(sv_a.get_SVA_report, None, sim_path, 'sva')
+        get_report_and_save(sva_zone.get_SVA_Zone_report, None, sim_path, 'sva_Zone')
+        get_report_and_save(beps.get_BEPS_report, None, sim_path, 'beps')
+        get_report_and_save(bepu.get_BEPU_report, None, sim_path, 'bepu')
+        get_report_and_save(lv_b.get_LVB_report, None, sim_path, 'lvb')
+        get_report_and_save(ps_e.get_PSE_report, None, sim_path, 'pse')
+        get_report_and_save(ps_f.get_PSF_report, None, sim_path, 'psf')
+        
+        st.success("SIM Parsed Successfully!!")
     else:
         st.error("Please upload a SIM file.")
