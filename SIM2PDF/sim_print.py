193c0b7b5d449af01a434611f33d1bed158a7cb5
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-27 15:20:45 +0530

    Update sim_print.py

--- SIM2PDF/sim_print.py
+++ SIM2PDF/sim_print.py
@@ -13,7 +13,6 @@
 #             st.error("Invalid directory path.")
 
 import os
-import streamlit as st
 import shutil
 from fpdf import FPDF
 from SIM2PDF.src_pdf import readSim
@@ -21,12 +20,18 @@ import PyPDF2
 
 def main(input_sim_files, reports):
     try:
-        print("Received directory path:", input_sim_files)  # Debugging output
-        if os.path.exists(input_sim_files):
+        # Normalize the path
+        normalized_path = os.path.normpath(input_sim_files)
+        print("Normalized directory path:", normalized_path)  # Debugging output
+
+        # Print the current working directory for additional context
+        print("Current working directory:", os.getcwd())
+
+        if os.path.exists(normalized_path):
             print("Directory path exists.")
-            if os.path.isdir(input_sim_files):
+            if os.path.isdir(normalized_path):
                 print("Directory path is valid.")
-                readSim.extractReport(input_sim_files, reports)
+                readSim.extractReport(normalized_path, reports)
                 print("PDFs Generated Successfully!")
                 return "PDFs Generated Successfully!"
             else:
