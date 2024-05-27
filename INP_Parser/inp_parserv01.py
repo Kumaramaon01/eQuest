7a3c92628b796aa0636be2fd7302857b8bdaa959
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-27 11:15:43 +0530

    Update inp_parserv01.py

--- INP_Parser/inp_parserv01.py
+++ INP_Parser/inp_parserv01.py
@@ -4,27 +4,37 @@ import streamlit as st
 from INP_Parser.src_inp import hvac_system
 
 def get_report_and_save(report_function, inp_path, file_suffix):
-    report = report_function(inp_path)
-    # Get the parent directory of the INP file
-    parent_directory = os.path.dirname(inp_path)
-    file_name = os.path.splitext(os.path.basename(inp_path))[0]
-    file_path = os.path.join(parent_directory, f'{file_name}_{file_suffix}.csv')
-    if os.path.isfile(file_path):
-        os.remove(file_path)
-    report.to_csv(file_path, index=False)
-    st.success(f"{file_suffix} Report Generated!")
+    try:
+        report = report_function(inp_path)
+        # Get the parent directory of the INP file
+        parent_directory = os.path.dirname(inp_path)
+        file_name = os.path.splitext(os.path.basename(inp_path))[0]
+        file_path = os.path.join(parent_directory, f'{file_name}_{file_suffix}.csv')
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+        report.to_csv(file_path, index=False)
+        st.success(f"{file_suffix} Report Generated!")
+    except Exception as e:
+        st.error(f"Error generating {file_suffix} report: {e}")
 
 def main(uploaded_file):
     if uploaded_file is not None:
-        # Save the uploaded file temporarily
-        inp_path = os.path.join(os.path.expanduser("~"), "Downloads", uploaded_file.name)
-        with open(inp_path, "wb") as f:
-            f.write(uploaded_file.getbuffer())
-        
-        # Generate reports
-        get_report_and_save(hvac_system.get_HVAC_System_report, inp_path, 'Sys_INP')
-        get_report_and_save(hvac_system.get_HVAC_Zone_report, inp_path, 'Zone_INP')
-        st.success("INP Parsed Successfully!!")
+        try:
+            # Save the uploaded file temporarily
+            inp_path = os.path.join(os.path.expanduser("~"), "Downloads", uploaded_file.name)
+            st.info(f"Saving uploaded file to {inp_path}")
+            
+            with open(inp_path, "wb") as f:
+                f.write(uploaded_file.getbuffer())
+            
+            # Generate reports
+            get_report_and_save(hvac_system.get_HVAC_System_report, inp_path, 'Sys_INP')
+            get_report_and_save(hvac_system.get_HVAC_Zone_report, inp_path, 'Zone_INP')
+            st.success("INP Parsed Successfully!!")
 
-        # Optionally, clean up the temp file after processing
-        os.remove(inp_path)
+            # Optionally, clean up the temp file after processing
+            os.remove(inp_path)
+        except FileNotFoundError as fnf_error:
+            st.error(f"FileNotFoundError: {fnf_error}")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
