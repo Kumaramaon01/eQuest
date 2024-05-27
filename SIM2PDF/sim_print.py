93ba864ab92091f0a4490bd7f2957325039aa377
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-27 13:54:30 +0530

    Update sim_print.py

--- SIM2PDF/sim_print.py
+++ SIM2PDF/sim_print.py
@@ -1,13 +1,27 @@
+# import os
+# import streamlit as st
+# import shutil
+# from fpdf import FPDF
+# from SIM2PDF.src_pdf import readSim
+# import PyPDF2
+
+# def main(input_sim_files, reports):
+#         if os.path.isdir(input_sim_files):
+#             readSim.extractReport(input_sim_files, reports)
+#             st.success("PDFs Generated Successfully!")
+#         else:
+#             st.error("Invalid directory path.")
+
 import os
-import streamlit as st
 import shutil
 from fpdf import FPDF
-from SIM2PDF.src_pdf import readSim
 import PyPDF2
 
 def main(input_sim_files, reports):
-        if os.path.isdir(input_sim_files):
-            readSim.extractReport(input_sim_files, reports)
-            st.success("PDFs Generated Successfully!")
-        else:
-            st.error("Invalid directory path.")
+    if os.path.isdir(input_sim_files):
+        readSim.extractReport(input_sim_files, reports)
+        print("PDFs Generated Successfully!")
+    else:
+        print("Invalid directory path.")
+
+# The rest of your sim_print.py code remains unchanged
