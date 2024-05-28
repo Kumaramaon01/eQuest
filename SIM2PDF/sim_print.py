ed62ae634ef8664e0c27fdddf9c1cea41a4701f1
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-28 12:27:53 +0530

    Update sim_print.py

--- SIM2PDF/sim_print.py
+++ SIM2PDF/sim_print.py
@@ -4,9 +4,6 @@ from SIM2PDF.src_pdf import readSim
 
 def main(input_sim_files, reports):
 
-    if os.path.isdir(input_sim_files):
-        st.success(f"The provided path '{input_sim_files}' exists and is a directory.")
-        result_message = readSim.extractReport(input_sim_files, reports)
-        st.success(result_message)
-    else:
-        st.error(f"The provided path '{input_sim_files}' does not exist or is not a directory.")
+    st.success(f"The provided path '{input_sim_files}' exists and is a directory.")
+    result_message = readSim.extractReport(input_sim_files, reports)
+    st.success(result_message)
