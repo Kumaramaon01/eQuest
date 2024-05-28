01b25c15c227945827f6935ec2e918e79b89b20b
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-28 11:29:26 +0530

    Update sim_print.py

--- SIM2PDF/sim_print.py
+++ SIM2PDF/sim_print.py
@@ -3,12 +3,9 @@ import streamlit as st
 from SIM2PDF.src_pdf import readSim
 
 def main(input_sim_files, reports):
-    # Validate path
-    st.success(input_sim_files)
-    st.success(reports)
-    
     if not os.path.isabs(input_sim_files):
         input_sim_files = os.path.abspath(input_sim_files)
+        st.success(input_sim_files)
     
     # Check if the path exists and is a directory
     if os.path.exists(input_sim_files):
