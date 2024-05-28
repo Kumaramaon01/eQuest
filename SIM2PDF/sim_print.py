e074f75a51900efa02b109df0709957dfdc54f28
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-28 11:16:12 +0530

    Update sim_print.py

--- SIM2PDF/sim_print.py
+++ SIM2PDF/sim_print.py
@@ -13,48 +13,11 @@ def main(input_sim_files, reports):
     # Check if the path exists and is a directory
     if os.path.exists(input_sim_files):
         if os.path.isdir(input_sim_files):
-            readSim.extractReport(input_sim_files, reports)
-            st.success("PDFs Generated Successfully!")
+            result_message = readSim.extractReport(input_sim_files, reports)
+            st.success(result_message)
         else:
             st.error("The path exists but is not a directory.")
             print("Error: The path exists but is not a directory.")
     else:
         st.error("Invalid directory path.")
         print("Error: Invalid directory path.")
-
-
-
-# import os
-# import shutil
-# from fpdf import FPDF
-# from SIM2PDF.src_pdf import readSim
-# import PyPDF2
-
-# def main(input_sim_files, reports):
-#     try:
-#         # Normalize the path
-#         normalized_path = os.path.normpath(input_sim_files)
-#         print("Normalized directory path:", normalized_path)  # Debugging output
-
-#         # Print the current working directory for additional context
-#         print("Current working directory:", os.getcwd())
-
-#         if os.path.exists(normalized_path):
-#             print("Directory path exists.")
-#             if os.path.isdir(normalized_path):
-#                 print("Directory path is valid.")
-#                 readSim.extractReport(normalized_path, reports)
-#                 print("PDFs Generated Successfully!")
-#                 return "PDFs Generated Successfully!"
-#             else:
-#                 error_message = "Path exists but is not a directory."
-#                 print(error_message)
-#                 return error_message
-#         else:
-#             error_message = "Path does not exist."
-#             print(error_message)
-#             return error_message
-#     except Exception as e:
-#         error_message = f"Error: {e}"
-#         print(error_message)
-#         return error_message
