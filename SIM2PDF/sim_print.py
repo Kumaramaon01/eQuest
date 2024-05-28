5f4e64bdc6d589a7e88ccfc94373bcb17f8dab5d
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-28 14:27:12 +0530

    Update sim_print.py

--- SIM2PDF/sim_print.py
+++ SIM2PDF/sim_print.py
@@ -1,11 +1,13 @@
 import os
 import streamlit as st
+import shutil
+from fpdf import FPDF
 from SIM2PDF.src_pdf import readSim
+import PyPDF2
 
 def main(input_sim_files, reports):
-    try:
-        st.success(f"The provided path '{input_sim_files.name}' exists and is a directory.")
-        result_message = readSim.extractReport(input_sim_files.name, reports)
-        st.success(result_message)
-    except Exception as e:
-        st.error(f"Error occurred: {str(e)}")
+    if os.path.isdir(input_sim_files):
+        readSim.extractReport(input_sim_files, reports)
+        st.success("PDFs Generated Successfully!")
+    else:
+        st.error("Invalid directory path.")
