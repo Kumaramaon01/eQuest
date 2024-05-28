2adf2db4383f5e160d02407ea2ea69d04a294078
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-28 10:47:55 +0530

    Update perge.py

--- Perging_INP/perge.py
+++ Perging_INP/perge.py
@@ -33,6 +33,7 @@ def update_inp_file(uploaded_file):
                 return updated_file_path  # Return the path of the updated INP file
         except Exception as e:
             st.error(f"An error occurred while updating INP file: {e}")
+            return None
 
 def main(uploaded_file):
     updated_file_path = update_inp_file(uploaded_file)
@@ -42,7 +43,7 @@ def main(uploaded_file):
         with open(updated_file_path, 'rb') as f:
             st.download_button(
                 label="Download Updated INP",
-                data=f,
+                data=f.read(),
                 file_name=os.path.basename(updated_file_path),
                 mime='text/plain'
             )
