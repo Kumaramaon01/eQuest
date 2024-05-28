623b1fa9bff101db9bc722afa15aa67d0c9aed91
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-28 13:02:41 +0530

    Update sim_print.py

--- SIM2PDF/sim_print.py
+++ SIM2PDF/sim_print.py
@@ -3,7 +3,9 @@ import streamlit as st
 from SIM2PDF.src_pdf import readSim
 
 def main(input_sim_files, reports):
-
-    st.success(f"The provided path '{input_sim_files}' exists and is a directory.")
-    result_message = readSim.extractReport(input_sim_files, reports)
-    st.success(result_message)
+    try:
+        st.success(f"The provided path '{input_sim_files.name}' exists and is a directory.")
+        result_message = readSim.extractReport(input_sim_files.name, reports)
+        st.success(result_message)
+    except Exception as e:
+        st.error(f"Error occurred: {str(e)}")
