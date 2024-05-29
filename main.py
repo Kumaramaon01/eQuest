a6123373f887c9ae9bdd53ca4fe46c9aac1f03a0
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-29 16:33:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ def main():
                 st.success(uploaded_sim_file)
 
                 # Run baseline automation
-                baselineAuto.main(uploaded_inp_file.name, uploaded_sim_file.name, input_climate, input_building_type, input_area, number_floor, heat_type)
+                baselineAuto.main(uploaded_inp_file, uploaded_sim_file, input_climate, input_building_type, input_area, number_floor, heat_type)
                 st.success("Baseline automation run successfully.")
             else:
                 st.error("Please upload both INP and SIM files.")
