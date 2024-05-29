0dece78fbd2ad56a7b2ad3c33ee8e13d39888da8
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-29 17:02:34 +0530

    Update update_MLC.py

--- BaselineAutomation/src/update_MLC.py
+++ BaselineAutomation/src/update_MLC.py
@@ -66,8 +66,9 @@ def get_system_path(building_type, heat_type, area, floor):
 def insert_material_data(climate_zone_file, amenity_file):
     start_marker1 = "= MATERIAL"
     end_marker1 = ".."
-    climate_zone_file = r"D:\EDS\S2302 eQuest Automation\S2302.1 Baseline Automation\S2302.1_GIT\" + climate_zone_file
+    climate_zone_file = "D:\\EDS\\S2302 eQuest Automation\\S2302.1 Baseline Automation\\S2302.1_GIT\\" + climate_zone_file
     st.success(climate_zone_file)
+
 
     with open(climate_zone_file, 'r') as file:
         data_climate_zone = file.readlines()
