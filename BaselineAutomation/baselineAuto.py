ccdfbdc5a6efe0b864ba39936241f416e33daf9f
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-29 16:04:57 +0530

    Update baselineAuto.py

--- BaselineAutomation/baselineAuto.py
+++ BaselineAutomation/baselineAuto.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from BaselineAutomation.src import update_MLC, insertConst, insertGlass, wwr, updateHVAC, HVAC_sys, perging, CLM_delete, update_lpd, updateFreshAir
 
-def main(input_inp_file, input_sim_file, input_climate, input_building_type, input_area, number_floor, heat_type):
+def main(input_inp_path, input_sim_path, input_climate, input_building_type, input_area, number_floor, heat_type):
     # Convert input_climate to an integer
     input_climate = int(input_climate)
     input_building_type = int(input_building_type)
@@ -10,9 +10,8 @@ def main(input_inp_file, input_sim_file, input_climate, input_building_type, inp
     floor = int(number_floor)
     heat_type = int(heat_type)
 
-    st.success(input_climate)
-    st.success(input_building_type)
-    st.success(input_area)
+    st.success(input_inp_path)
+    st.success(input_sim_path)
     
     if input_climate < 1 or input_climate > 8 or input_building_type > 1 or input_building_type < 0:
         st.success("Climate input or Building type is Wrong!\n")
