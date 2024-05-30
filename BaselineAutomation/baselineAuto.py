a9c50f5dd264151fb8bd75a441ffc3e089a1166a
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-30 15:05:07 +0530

    Update baselineAuto.py

--- BaselineAutomation/baselineAuto.py
+++ BaselineAutomation/baselineAuto.py
@@ -1,4 +1,5 @@
 import os
+import streamlit as st
 from BaselineAutomation.src import update_MLC, insertConst, insertGlass, wwr, updateHVAC, HVAC_sys, perging, CLM_delete, update_lpd, updateFreshAir
 
 def main(input_inp_path, sim_content, input_climate, input_building_type, input_area, number_floor, heat_type, output_file):
