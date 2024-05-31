3012ca1c2f8833afb4b36619ff1c161daea47f46
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-31 15:18:33 +0530

    Update sim_print.py

--- SIM2PDF/sim_print.py
+++ SIM2PDF/sim_print.py
@@ -1,19 +1,21 @@
 import os
-import streamlit as st
-from fpdf import FPDF
-from SIM2PDF.src_pdf import readSim
 import tempfile
+import streamlit as st
+from SIM2PDF.src_pdf import readSim
 
 def main(reports, uploaded_file):
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        # Save the uploaded file to a temporary directory
-        temp_sim_path = os.path.join(tmpdirname, uploaded_file.name)
-        with open(temp_sim_path, 'wb') as temp_file:
-            temp_file.write(uploaded_file.read())
-        
-        st.success("File uploaded and saved to temporary directory.")
-        
-        # Extract reports and generate PDFs
-        readSim.extractReport(temp_sim_path, reports)
-        
-        st.success("PDFs Generated Successfully!")
+    try:
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            # Save the uploaded file to a temporary directory
+            temp_sim_path = os.path.join(tmpdirname, uploaded_file.name)
+            with open(temp_sim_path, 'wb') as temp_file:
+                temp_file.write(uploaded_file.read())
+            
+            st.success(f"File uploaded and saved to temporary directory: {temp_sim_path}")
+            
+            # Extract reports and generate PDFs
+            readSim.extractReport(temp_sim_path, reports)
+            
+            st.success("PDFs Generated Successfully!")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
