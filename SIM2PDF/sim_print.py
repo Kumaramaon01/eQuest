caa7054d16f0211bbd6cd23f417ccbd5ad1106d4
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-05-31 15:20:09 +0530

    Update sim_print.py

--- SIM2PDF/sim_print.py
+++ SIM2PDF/sim_print.py
@@ -1,28 +1,14 @@
 import os
-import tempfile
 import streamlit as st
+import shutil
+from fpdf import FPDF
 from SIM2PDF.src_pdf import readSim
+import PyPDF2
 
-def main(reports, uploaded_files):
-    try:
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            if not isinstance(uploaded_files, list):
-                uploaded_files = [uploaded_files]
-            
-            # Save uploaded files to temporary directory
-            temp_sim_paths = []
-            for uploaded_file in uploaded_files:
-                temp_sim_path = os.path.join(tmpdirname, uploaded_file.name)
-                with open(temp_sim_path, 'wb') as temp_file:
-                    temp_file.write(uploaded_file.read())
-                temp_sim_paths.append(temp_sim_path)
-
-            st.success(f"Files uploaded and saved to temporary directory: {tmpdirname}")
-            
-            # Extract reports and generate PDFs
-            for sim_path in temp_sim_paths:
-                readSim.extractReport(sim_path, reports)
-            
-            st.success("PDFs Generated Successfully!")
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+def main(input_sim_files, reports):
+    if os.path.isdir(input_sim_files):
+        st.success(input_sim_files)
+        readSim.extractReport(input_sim_files, reports)
+        st.success("PDFs Generated Successfully!")
+    else:
+        st.error("Invalid directory path.")
