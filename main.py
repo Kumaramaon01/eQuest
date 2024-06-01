a978e1161f9f540ff351b36e72fbe7f9d4b9b2ae
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-01 21:31:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -100,22 +100,7 @@ def main():
     # Add icon and tooltip to col3
 
     # Navigation bar with buttons below the header
-    # Custom HTML and CSS for the gold chain line
-    gold_chain_line = """
-    <style>
-        .gold-chain {
-            width: 100%;
-            height: 20px;
-            background-image: url('https://i.imgur.com/your-gold-chain-image-url.png');
-            background-repeat: repeat-x;
-        }
-    </style>
-    <div class="gold-chain"></div>
-    """
-    
-    # Inject the custom HTML and CSS
-    st.markdown(gold_chain_line, unsafe_allow_html=True)
-
+    st.markdown('<hr style="border:2px solid red">', unsafe_allow_html=True)
     
     col3, col4, col5, col6 = st.columns([1, 1, 1, 1])
     with col3:
