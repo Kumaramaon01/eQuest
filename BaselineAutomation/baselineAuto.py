65395a1e90168d802d3840b8fdc7381c7a430580
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-04 20:10:59 +0530

    Update baselineAuto.py

--- BaselineAutomation/baselineAuto.py
+++ BaselineAutomation/baselineAuto.py
@@ -1,8 +1,22 @@
 import os
 import streamlit as st
+import tempfile
 from BaselineAutomation.src import update_MLC, insertConst, insertGlass, wwr, updateHVAC, HVAC_sys, perging, CLM_delete, update_lpd, updateFreshAir
 
-def getInp(input_inp_path, sim_content, input_climate, input_building_type, input_area, number_floor, heat_type):
+def getInp(input_inp_path, sim_file_path, input_climate, input_building_type, input_area, number_floor, heat_type):
+
+    if input_inp_path is not None:
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(input_inp_path.getbuffer())
+            temp_file_path = temp_file.name
+        inp_path = temp_file_path
+
+    if sim_file_path is not None:
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(sim_file_path.getbuffer())
+            temp_file_path = temp_file.name
+        sim_path = temp_file_path
+
     # Convert inputs to appropriate types
     input_climate = int(input_climate)
     input_building_type = int(input_building_type)
@@ -11,50 +25,44 @@ def getInp(input_inp_path, sim_content, input_climate, input_building_type, inpu
     heat_type = int(heat_type)
 
     if input_climate < 1 or input_climate > 8 or input_building_type > 1 or input_building_type < 0:
-        print("Climate input or Building type is Wrong!\n")
+        st.error("Error: Climate input or Building type is Wrong!\n")
         return
 
     # Get climate and system paths
     climate_path = update_MLC.get_climate_path(input_climate, input_building_type)
     system_path = update_MLC.get_system_path(input_building_type, heat_type, input_area, number_floor)
-    st.success("inside getInp")
-    # input_inp_path = os.path.dirname(input_inp_path)
-    st.success(input_inp_path)
-    st.success(system_path)
 
     # Convert paths to absolute paths
-    climate_path = os.path.dirname(climate_path)
-    st.success(climate_path)
-    if os.path.isfile(input_inp_path):
-        st.success("inside if")
-        st.success(input_inp_path)
-        # Your other code here
-        mat_data = update_MLC.insert_material_data(climate_path, input_inp_path)
-        print("\nInserted Material Data")
+    climate_path = os.path.abspath(climate_path)
+    # climate_path = "D:\EDS\Test Projects\web equest\BaselineAutomation\library\Residential_Construction\Climate zone 1.inp"
+
+    if os.path.isfile(inp_path):
+        mat_data = update_MLC.insert_material_data(climate_path, inp_path)
+        st.success("Inserted Material Data")
         lyr_data = update_MLC.insert_layers_data(climate_path, mat_data)
-        print("Inserted Layer Data")
+        st.success("Inserted Layer Data")
         const_data = update_MLC.insert_const_data(climate_path, lyr_data)
-        print("Construction Data Inserted")
+        st.success("Construction Data Inserted")
         update_ConstName = insertConst.update_external_wall_roof_undergrnd(const_data)
-        print("Construction name based on Wall, roof and underground is updated")
+        st.success("Construction name based on Wall, roof and underground is updated")
         updateGlass = insertGlass.update_glass(climate_path, update_ConstName)
-        print("Inserted Glass Data")
+        st.success("Inserted Glass Data")
         updateGlassType = insertGlass.update_glass_type(updateGlass)
-        print("Glass-Type Data is Updated by All Win")
-        updateWWR = wwr.UpdateWWR(sim_content, updateGlassType)
-        print("Updated WWR")
+        st.success("Glass-Type Data is Updated by All Win")
+        updateWWR = wwr.UpdateWWR(sim_path, updateGlassType)
+        st.success("Updated WWR")
         modifyHVAC = updateHVAC.HVAC_Modification(updateWWR)
-        print("HVAC_Updated")
+        st.success("HVAC_Updated")
         hvac_sys = HVAC_sys.systems(modifyHVAC, system_path)
-        print("System_updated")
+        st.success("System_updated")
         
         value = system_path.split(".inp")[0][-1]
         if value in ['1', '2', '3', '4']:
             update_zone = HVAC_sys.modify_conditioned(hvac_sys, system_path)
-            print("Conditioned_zone updated")
+            st.success("Conditioned_zone updated")
         else:
             update_zone = HVAC_sys.modify_floor(hvac_sys, system_path)
-            print("Floor updated")
+            st.success("Floor updated")
         
         ###### removing unique value from data or perging ######
         perge_data_annual = perging.perging_data_annual(update_zone)
@@ -65,23 +73,32 @@ def getInp(input_inp_path, sim_content, input_climate, input_building_type, inpu
         material_delete = CLM_delete.perging_data_material(layers_delete)
     
     ######################################################################################
-        modify_lpd = update_lpd.updateLPD(material_delete, sim_content)
-        print("LPD Updated")
-    
-        modify_freshAir = updateFreshAir.updateBCVentilation(modify_lpd, sim_content)
-        print("FreshAir Updated!!\n")
-    
+        modify_lpd = update_lpd.updateLPD(material_delete, sim_path)
+        st.success("LPD Updated")
+
+        modify_freshAir = updateFreshAir.updateBCVentilation(modify_lpd, sim_path)
+        st.success("FreshAir Updated!!\n")
+
     ######################################################################################
     ################################## CLEAN INP FILE ####################################
     ######################################################################################
          
+        directory_path, filename = os.path.split(inp_path)
+        new_filename = filename.replace(".inp", "_Baseline_Automation.inp")
+        input_inp_ = input_inp_path.name.split('.')[0]
+
         # Write modified inp file 
-        with open(input_inp_path, 'w') as file:
+        with open(new_filename, 'w') as file:
             file.writelines(modify_freshAir)
 
-        # Provide a success message
-        st.success("INP file has been successfully updated.")
+        # Provide download link for the zip file
+        with open(new_filename, 'rb') as f:
+            st.download_button(
+                label="Download Updated INP",
+                data=f,
+                file_name=f"{os.path.basename(input_inp_)}_Baseline_Automation.inp",
+            )
 
 if __name__ == "__main__":
-    # Replace arguments with appropriate values
-    getInp(input_inp_path, sim_content, input_climate, input_building_type, input_area, number_floor, heat_type)
+    # You can add code here to accept input from the command line if desired
+    pass
