96be9e87accaeb94b95fa107886afdad15c5720a
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-10 12:15:33 +0530

    Update insertConst.py

--- BaselineAutomation/src/insertConst.py
+++ BaselineAutomation/src/insertConst.py
@@ -2,7 +2,6 @@ import pandas as pd
 import os
 import re
 
-# function to update Construction wall under external-wall and under underground section
 def update_external_wall_roof_undergrnd(data):
     start_marker = "Floors / Spaces / Walls / Windows / Doors"
     end_marker = "Electric & Fuel Meters"
@@ -27,19 +26,18 @@ def update_external_wall_roof_undergrnd(data):
             index = line.find('=')
             if index != -1:
                 value_before_equal_wall = line[:index].strip()
-        elif "BL Roof" in line:
-            index = line.find('=')
-            if index != -1:
-                value_before_equal_roof = line[:index].strip()
         elif "Undergrd W" in line:
             index = line.find('=')
             if index != -1:
                 value_before_equal_under = line[:index].strip()
+        elif "BL Roof" in line:
+            index = line.find('=')
+            if index != -1:
+                value_before_equal_roof = line[:index].strip()
 
     if start_index is not None and end_index is not None:
         inside_exterior_wall = False
         inside_underground_wall = False
-        inside_exterior_wall1 = False
 
         for line_index in range(start_index + 3, end_index - 4):
             line = data[line_index]
@@ -47,23 +45,21 @@ def update_external_wall_roof_undergrnd(data):
             if "EXTERIOR-WALL" in line:
                 inside_exterior_wall = True
                 inside_underground_wall = False
-                inside_exterior_wall1 = True
         
             elif "UNDERGROUND-WALL" in line:
                 inside_underground_wall = True
                 inside_exterior_wall = False
-                inside_exterior_wall1 = False
             
             elif inside_exterior_wall:
                 if ".." in line:
                     inside_exterior_wall = False
                 elif "CONSTRUCTION" in line:
                     construction_value = re.search(r'CONSTRUCTION\s*=\s*(\S+)', line).group(1)
-                    if "TOP" not in construction_value:
+                    if "TOP" not in construction_value and "extroof" not in line:
                         data[line_index] = re.sub(r'CONSTRUCTION\s*=\s*(\S+)', r'CONSTRUCTION     = {}'.format(value_before_equal_wall), line)
-                    elif "TOP" in construction_value:
-                        data[line_index] = re.sub(r'CONSTRUCTION\s*=\s*(\S+)', r'CONSTRUCTION     = {}'.format(value_before_equal_roof), line)
-            
+                    elif "extroof" in construction_value:
+                        data[line_index] = re.sub(r'extroof', value_before_equal_roof, line)
+
             elif inside_underground_wall:
                 if ".." in line:
                     inside_underground_wall = False
