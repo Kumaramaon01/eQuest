ba9930efcfba8a8fa8b0e9df84befae804f3479c
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-10 15:46:23 +0530

    Update insertConst.py

--- BaselineAutomation/src/insertConst.py
+++ BaselineAutomation/src/insertConst.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import os
 import re
-import streamlit as st
 
 def update_external_wall_roof_undergrnd(data):
     start_marker = "Floors / Spaces / Walls / Windows / Doors"
@@ -35,10 +34,7 @@ def update_external_wall_roof_undergrnd(data):
             index = line.find('=')
             if index != -1:
                 value_before_equal_roof = line[:index].strip()
-    st.success(value_before_equal_wall)
-    st.success(value_before_equal_roof)
-    st.success(value_before_equal_under)
-    
+
     if start_index is not None and end_index is not None:
         inside_exterior_wall = False
         inside_underground_wall = False
@@ -58,18 +54,24 @@ def update_external_wall_roof_undergrnd(data):
                 if ".." in line:
                     inside_exterior_wall = False
                 elif "CONSTRUCTION" in line:
+                    if "extroof" in line:
+                        if value_before_equal_roof is not None:
+                            data[line_index] = re.sub(r'extroof', value_before_equal_roof, line)
+                    else:
+                        construction_value = re.search(r'CONSTRUCTION\s*=\s*"(\S+)"', line)
+                        if construction_value and value_before_equal_wall is not None:
+                            data[line_index] = line.replace(construction_value.group(1), value_before_equal_wall)
+                elif "LOCATION" in line:
                     location_value = re.search(r'LOCATION\s*=\s*(\S+)', line).group(1)
-                    construction_value = re.search(r'CONSTRUCTION\s*=\s*(\S+)', line).group(1)
                     if "TOP" not in location_value:
-                        data[line_index] = '   CONSTRUCTION     = {}\n'.format(value_before_equal_wall)
-                    elif "extroof" in construction_value or "Ext Roof" in construction_value or "Roof" in construction_value:
-                        data[line_index] = '   CONSTRUCTION     = {}\n'.format(value_before_equal_roof)
+                        data[line_index] = re.sub(r'LOCATION\s*=\s*(\S+)', r'LOCATION         = TOP', line)
 
             elif inside_underground_wall:
                 if ".." in line:
                     inside_underground_wall = False
-                elif "CONSTRUCTION" in line and value_before_equal_under is not None:
+                elif "CONSTRUCTION" in line:
                     if "BOTTOM" not in data[line_index + 1]:
-                        data[line_index] = '   CONSTRUCTION     = {}\n'.format(value_before_equal_under)
+                        if value_before_equal_under is not None:
+                            data[line_index] = '   CONSTRUCTION     = "{}"\n'.format(value_before_equal_under)
 
-    return data
+    return data  
