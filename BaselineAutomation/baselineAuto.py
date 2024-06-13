d5d10200bfabaa808e36c8808c05321389e57baf
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-13 12:27:24 +0530

    Update baselineAuto.py

--- BaselineAutomation/baselineAuto.py
+++ BaselineAutomation/baselineAuto.py
@@ -64,25 +64,25 @@ def getInp(input_inp_path, sim_file_path, input_climate, input_building_type, in
         st.success("Updated WWR if ratio > 0.4")
 
         # ######################################################## HVAC #########################################################
-        # modifyHVAC = updateHVAC.HVAC_Modification(updateWWR)
-        # st.success("HVAC_Updated (All System Deleted)")
-        # hvac_sys = HVAC_sys.systems(modifyHVAC, system_path)
-        # st.success("Data Replaces HVAC")
-        # value = system_path.split(".inp")[0][-1]
-        # if value in ['1', '2', '3', '4']:
-        #     update_zone = HVAC_sys.modify_conditioned(hvac_sys, system_path)
-        #     st.success("Conditioned_zone updated")
-        # else:
-        #     update_zone = HVAC_sys.modify_floor(hvac_sys, system_path)
-        #     st.success("Floor updated")
+        modifyHVAC = updateHVAC.HVAC_Modification(updateWWR)
+        st.success("HVAC_Updated (All System Deleted)")
+        hvac_sys = HVAC_sys.systems(modifyHVAC, system_path)
+        st.success("Data Replaces HVAC")
+        value = system_path.split(".inp")[0][-1]
+        if value in ['1', '2', '3', '4']:
+            update_zone = HVAC_sys.modify_conditioned(hvac_sys, system_path)
+            st.success("Conditioned_zone updated")
+        else:
+            update_zone = HVAC_sys.modify_floor(hvac_sys, system_path)
+            st.success("Floor updated")
     
-        # ######################################################## LPD #########################################################
-        # modify_lpd = update_lpd.updateLPD(update_zone, sim_path)
-        # st.success("LPD Updated")
+        ######################################################### LPD #########################################################
+        modify_lpd = update_lpd.updateLPD(update_zone, sim_path)
+        st.success("LPD Updated")
 
-        # ######################################################## FRESH AIR ###################################################
-        # modify_freshAir = updateFreshAir.updateBCVentilation(modify_lpd, sim_path)
-        # st.success("FreshAir Updated!!\n")
+        ######################################################### FRESH AIR ###################################################
+        modify_freshAir = updateFreshAir.updateBCVentilation(modify_lpd, sim_path)
+        st.success("FreshAir Updated!!\n")
          
         directory_path, filename = os.path.split(inp_path)
         new_filename = re.sub(r'\.inp?$', '_Baseline_Automation.inp', filename, flags=re.IGNORECASE)
@@ -90,7 +90,7 @@ def getInp(input_inp_path, sim_file_path, input_climate, input_building_type, in
         # updateWWR = updateWWR.replace('\n', '\r\n')
         # Write modified inp file 
         with open(new_filename, 'w',newline= '\r\n') as file:
-            file.writelines(updateWWR)
+            file.writelines(modify_freshAir)
 
         with open(new_filename, 'rb') as f:
             st.download_button(
