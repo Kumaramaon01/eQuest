1ccb6a63d6a78a6ffe868dd7706047528b6df065
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-14 10:33:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -391,9 +391,9 @@ st.markdown("""
 
 st.markdown("""
     <div class="social-media">
-        <a href="https://twitter.com/your_profile" target="_blank"><i class="fab fa-twitter"></i></a>
-        <a href="https://www.facebook.com/your_profile" target="_blank"><i class="fab fa-facebook"></i></a>
-        <a href="https://www.instagram.com/your_profile" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a href="https://https://x.com/edsglobal?lang=en" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.facebook.com/Environmental.Design.Solutions/" target="_blank"><i class="fab fa-facebook"></i></a>
+        <a href="https://www.instagram.com/eds_global/?hl=en" target="_blank"><i class="fab fa-instagram"></i></a>
         <a href="https://www.linkedin.com/company/environmental-design-solutions/" target="_blank"><i class="fab fa-linkedin"></i></a>
     </div>
     """, unsafe_allow_html=True)
