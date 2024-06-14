27a74b433984b199c80979170e8325fed0c709b9
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-14 18:47:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -217,7 +217,7 @@ def main():
         col1, col2 = st.columns([1,1])
         with col1:
            st.audio("crash.mp3", format="audio/mpeg", loop=True)
-           st.audio("crash.mp3", format="audio/mpeg", loop=True)
+           st.audio("song.oga", format="audio/mpeg", loop=True)
 
         with col2:
             gif_path = "images/Translational_motion.gif"
