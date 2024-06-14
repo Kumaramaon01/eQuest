a2d2e5fedcbd3a4da0f82838ad540e25f833400b
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-14 12:57:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -190,22 +190,8 @@ def main():
         """, unsafe_allow_html=True)
         col1, col2 = st.columns([1,1])
         with col1:
-            st.markdown("""
-            <b>Design and Visuals:</b> Random motion of 
-            particles suspended in a fluid (liquid or gas) resulting from their collision with the fast-moving molecules in the fluid.<br>
-            """, unsafe_allow_html=True)
-
-            audio_file = open("crash.mp3", "rb")
-            audio_bytes = audio_file.read()
-            st.audio(audio_bytes, format="audio/ogg")
-            sample_rate = 44100  # 44100 samples per second
-            seconds = 2  # Note duration of 2 seconds
-            frequency_la = 440  # Our played note will be 440 Hz
-            # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
-            t = np.linspace(0, seconds, seconds * sample_rate, False)
-            # Generate a 440 Hz sine wave
-            note_la = np.sin(frequency_la * t * 2 * np.pi)
-            st.audio(note_la, sample_rate=sample_rate)
+            gif_path = "images/Translational_motion.gif"
+            st.image(gif_path, use_column_width=True)
 
         with col2:
             # st.markdown("""
