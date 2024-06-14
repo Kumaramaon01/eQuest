788f6a547d1daf1b11e09207b6c003593c58b8c3
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-14 14:29:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -366,7 +366,7 @@ st.markdown("""
     .social-media a {
         margin: 0 10px;
         text-decoration: none;
-        color: inherit;
+        color: blue;
     }
     .social-media i {
         font-size: 24px;
@@ -379,7 +379,8 @@ st.markdown("""
 st.markdown(
     """
         <div class="social-media" style="margin-top: 10px;">
-            <p>Designed and Developed by <a href="https://edsglobal.com" target="_blank"><b style="text-color: red;">Environmental Design Solutions Pvt. Ltd</b></a></p>
+            <p>Designed and Developed by <a href="https://edsglobal.com" target="_blank"><b style="text-color: red;"><u>Environmental Design Solutions Pvt. Ltd</u>
+            </b></a></p>
             <a href="https://twitter.com/edsglobal?lang=en" target="_blank"><i class="fab fa-twitter"></i></a>
             <a href="https://www.facebook.com/Environmental.Design.Solutions/" target="_blank"><i class="fab fa-facebook"></i></a>
             <a href="https://www.instagram.com/eds_global/?hl=en" target="_blank"><i class="fab fa-instagram"></i></a>
