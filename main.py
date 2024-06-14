533918e6443bfdd0c682a0f5ed0a0b8779a5fa2f
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-14 14:54:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -191,6 +191,9 @@ def main():
         col1, col2 = st.columns([1,1])
         with col1:
             st.audio("crash.mp3", format="audio/mpeg", loop=True)
+            st.markdown("""
+            <h3 style="color:red;"></h3>
+            """, unsafe_allow_html=True)
             video_file = open('star.mp4', 'rb')
             video_bytes = video_file.read()
             st.video(video_bytes)
