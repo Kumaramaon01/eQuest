fd8a035bd16969fade03ddc12f01cbaf8279619d
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-15 01:38:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -221,7 +221,7 @@ def main():
 
         with col2:
             gif_path = "images/Translational_motion.gif"
-            st.image(gif_path, width=80)
+            st.image(gif_path, width=130)
 
     elif st.session_state.script_choice == "INP Parser":
         st.markdown("""
