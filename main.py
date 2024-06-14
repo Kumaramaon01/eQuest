66f50da0f923a4131903db228c259a9e0358afce
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-14 14:51:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -191,7 +191,7 @@ def main():
         col1, col2 = st.columns([1,1])
         with col1:
             st.audio("crash.mp3", format="audio/mpeg", loop=True)
-            video_file = open('myvideo.mp4', 'rb')
+            video_file = open('star.mp4', 'rb')
             video_bytes = video_file.read()
             st.video(video_bytes)
 
