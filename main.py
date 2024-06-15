e1065af8b5606e9d1763bf741f3cec84a711154e
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-15 09:27:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ def main():
         unsafe_allow_html=True
     )
 
-    col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11 = st.columns([1, 1, 1, 1, 1, 1, 1, 1.6, 1, 0.7, 0.7])
+    col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11 = st.columns([0.5, 1, 1, 1, 1, 1, 1, 1.6, 1, 0.7, 0.7])
     with col1:
         if st.button("Home", key="buttons"):
             st.session_state.script_choice = "home"
