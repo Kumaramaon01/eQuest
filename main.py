54043cc4f46e73a21f5606038f7eccd0e57e28fc
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-15 14:54:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -341,14 +341,15 @@ def main():
                 sim_print.main(reports, uploaded_file)
                 
     elif st.session_state.script_choice == "ask":
-        col1, col2, col3 = st.columns(3)
+        col1, col2 = st.columns(2)
         with col1:
             st.write(icon_with_tooltip1, unsafe_allow_html=True)
             email = st.text_input("Enter your mail:")
-        with col2:
-            st.write(icon_with_tooltip2, unsafe_allow_html=True)
             user_input = st.text_area("Enter your Queries:")
-        with col3:
+        # with col2:
+        #     st.write(icon_with_tooltip2, unsafe_allow_html=True)
+        #     user_input = st.text_area("Enter your Queries:")
+        with col2:
             st.image("https://images.jdmagicbox.com/comp/delhi/k8/011pxx11.xx11.180809193209.h6k8/catalogue/environmental-design-solutions-vasant-vihar-delhi-environmental-management-consultants-leuub0bjnn.jpg")
     
         # Submit button
