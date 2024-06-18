c44244934d2725f925588683b14c2b5a5ecec04a
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-18 14:20:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def send_email(subject, message, from_email, to_email):
 button_style = """
     <style>
         .stButton>button {
-            box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.9);
+            box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.7);
         }
     </style>
 """
