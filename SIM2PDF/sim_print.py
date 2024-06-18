7b38132d987552290692e27604404a28bde8aa17
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-18 14:54:50 +0530

    Update sim_print.py

--- SIM2PDF/sim_print.py
+++ SIM2PDF/sim_print.py
@@ -6,9 +6,15 @@ from SIM2PDF.src_pdf import readSim
 import PyPDF2
 
 def main(input_sim_files, reports):
-    if os.path.isdir(input_sim_files):
-        st.success(input_sim_files)
-        readSim.extractReport(input_sim_files, reports)
+    if input_sim_files is not None:
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(input_sim_files.getbuffer())
+            temp_file_path = temp_file.name
+        sim_path = temp_file_path
+        
+    if os.path.isdir(sim_path):
+        st.success(sim_path)
+        readSim.extractReport(sim_path, reports)
         st.success("PDFs Generated Successfully!")
     else:
         st.error("Invalid directory path.")
