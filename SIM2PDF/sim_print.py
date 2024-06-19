1612ac8a15d5ad2946b4b06140f2791210af7467
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-19 10:24:36 +0530

    Update sim_print.py

--- SIM2PDF/sim_print.py
+++ SIM2PDF/sim_print.py
@@ -9,22 +9,28 @@ import tempfile
 def main(reports, input_sim_files):
     st.success("Inside sim_print.py")
     st.success(input_sim_files)
-    if input_sim_files is not None:
-        try:
-            with tempfile.NamedTemporaryFile() as temp_dir:
-                # Save the uploaded file temporarily
-                sim_path = os.path.join(temp_dir, input_sim_files.name)
-                with open(sim_path, "wb") as f:
-                    f.write(input_sim_files.getbuffer())
-                sim_path = sim_path.replace('\n', '\r\n')
-                
-                if os.path.isdir(sim_path):
-                    st.success(sim_path)
-                    # readSim.extractReport(sim_path, reports)
-                    st.success("PDFs Generated Successfully!")
-                else:
-                    st.error("Invalid directory path.")
+    
+    if input_sim_files is not None and isinstance(input_sim_files, list):
+        for input_sim_file in input_sim_files:
+            try:
+                with tempfile.TemporaryDirectory() as temp_dir:
+                    # Save the uploaded file temporarily
+                    sim_path = os.path.join(temp_dir, input_sim_file.name)
+                    with open(sim_path, "wb") as f:
+                        f.write(input_sim_file.getbuffer())
+                    sim_path = sim_path.replace('\n', '\r\n')
+
+                    if os.path.isdir(sim_path):
+                        st.success(sim_path)
+                        # readSim.extractReport(sim_path, reports)
+                        st.success("PDFs Generated Successfully!")
+                    else:
+                        st.error("Invalid directory path.")
             
-        except Exception as e:
-            st.error(f"An error occurred while updating SIM file: {e}")
-            return None, None
+            except Exception as e:
+                st.error(f"An error occurred while updating SIM file: {e}")
+    else:
+        st.error("No files uploaded or invalid input.")
+
+# Example usage
+# main(reports, input_sim_files)  # You need to pass the actual parameters here
