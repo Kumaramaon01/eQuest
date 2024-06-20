64cafdcc9e8c86ed9577d56cde46be31323959b7
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-20 10:21:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -205,7 +205,7 @@ def main():
         unsafe_allow_html=True
     )
 
-    col2, col3, col4, col5, col6, col7, col8, col9, col10, col11 = st.columns([1, 1, 1, 1, 1, 1, 1.6, 1, 0.7, 0.7])
+    col2, col3, col4, col5, col6, col7, col8, col9, col10, col11 = st.columns([1, 1, 1, 1, 1, 1, 1.4, 1, 0.7, 0.8])
     with col2:
         if st.button("About EDS", key="buttons"):
             st.session_state.script_choice = "eds"
