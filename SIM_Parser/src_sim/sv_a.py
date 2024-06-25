ee4c41065ce5155ee0386cf7f86a5fb2b525393b
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-25 14:00:10 +0530

    Update sv_a.py

--- SIM_Parser/src_sim/sv_a.py
+++ SIM_Parser/src_sim/sv_a.py
@@ -28,45 +28,51 @@ def get_UnconditionedAboveArea(sva_df):
     return UnconditionedAbove_Area
 
 def get_SVA_report(name):
-    with open(name) as f:
-        flist = f.readlines()
-
-        sva_count = [] 
-        for num, line in enumerate(flist, 0):
-            if 'SV-A' in line:
-                sva_count.append(num)
-            if 'SS-D' in line:
-                numend = num
-        numstart = sva_count[0] 
-        sva_rpt = flist[numstart:numend]
-        
-        sva_str = []
-        for line in sva_rpt:
-            if (('SUM' in line and '.' in line) or ('PTAC' in line and '.' in line and 'WEATHER' not in line) or
-                ('VAVS' in line and '.' in line) or ('PIU' in line and '.' in line) or 
-                ('FC' in line and 'zn' not in line and 'Zn' not in line and '.' in line) or 
-                ('UVT' in line and '.' in line) or
-                ('PSZ' in line and '.' in line) or ('PMZS' in line and '.' in line)):
-                sva_str.append(line)
-
-        result = []  
-        for line in sva_str:
-            sva_list = []
-            splitter = line.split()
-            space_name = " ".join(splitter[:-10])
-            sva_list=splitter[-10:]
-            sva_list.insert(0,space_name)
-            result.append(sva_list)
-
-        sva_df = pd.DataFrame(result)
-        sva_df.columns = ['SYSTEM_TYPE', 'ALTITUDE_FACTOR', 'FLOOR_AREA(SQFT)',
-                        'MAX_PEOPLE', 'OUTSIDE_AIR_RATIO', 'COOLING_CAPACITY(KBTU/HR)',
-                        'SENSIBLE(SHR)', 'HEATING_CAPACITY(KBTU/HR)', 'COOLING_EIR(BTU/BTU)', 'HEATING_EIR(BTU/BTU)', 'HEAT_PUMP(SUPP_HEAT)(KBTU/HR)']
-        sva_df['FLOOR_AREA(SQFT)'] = pd.to_numeric(sva_df['FLOOR_AREA(SQFT)'])
-        sva_df.index.name = name
-        value_before_backslash = ''.join(reversed(name)).split("\\")[0]
-        name1 = ''.join(reversed(value_before_backslash))
-        name = name1.rsplit(".", 1)[0]
-        sva_df.insert(0, 'RUNNAME', name)
-     
-    return sva_df
+    try:
+        with open(name) as f:
+            flist = f.readlines()
+    
+            sva_count = [] 
+            for num, line in enumerate(flist, 0):
+                if 'SV-A' in line:
+                    sva_count.append(num)
+                if 'SS-D' in line:
+                    numend = num
+            numstart = sva_count[0] 
+            sva_rpt = flist[numstart:numend]
+            
+            sva_str = []
+            for line in sva_rpt:
+                if (('SUM' in line and '.' in line) or ('PTAC' in line and '.' in line and 'WEATHER' not in line) or
+                    ('VAVS' in line and '.' in line) or ('PIU' in line and '.' in line) or 
+                    ('FC' in line and 'zn' not in line and 'Zn' not in line and '.' in line) or 
+                    ('UVT' in line and '.' in line) or
+                    ('PSZ' in line and '.' in line) or ('PMZS' in line and '.' in line)):
+                    sva_str.append(line)
+    
+            result = []  
+            for line in sva_str:
+                sva_list = []
+                splitter = line.split()
+                space_name = " ".join(splitter[:-10])
+                sva_list=splitter[-10:]
+                sva_list.insert(0,space_name)
+                result.append(sva_list)
+    
+            sva_df = pd.DataFrame(result)
+            sva_df.columns = ['SYSTEM_TYPE', 'ALTITUDE_FACTOR', 'FLOOR_AREA(SQFT)',
+                            'MAX_PEOPLE', 'OUTSIDE_AIR_RATIO', 'COOLING_CAPACITY(KBTU/HR)',
+                            'SENSIBLE(SHR)', 'HEATING_CAPACITY(KBTU/HR)', 'COOLING_EIR(BTU/BTU)', 'HEATING_EIR(BTU/BTU)', 'HEAT_PUMP(SUPP_HEAT)(KBTU/HR)']
+            sva_df['FLOOR_AREA(SQFT)'] = pd.to_numeric(sva_df['FLOOR_AREA(SQFT)'])
+            sva_df.index.name = name
+            value_before_backslash = ''.join(reversed(name)).split("\\")[0]
+            name1 = ''.join(reversed(value_before_backslash))
+            name = name1.rsplit(".", 1)[0]
+            sva_df.insert(0, 'RUNNAME', name)
+         
+        return sva_df
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        columns = ['AZIMUTH', 'AVERAGE(U-VALUE/WINDOWS)(BTU/HR-SQFT-F)', 'AVERAGE(U-VALUE/WALLS)(BTU/HR-SQFT-F)', 'AVERAGE U-VALUE(WALLS+WINDOWS)(BTU/HR-SQFT-F)', 
+                              'WINDOW(AREA)(SQFT)', 'WALL(AREA)(SQFT)', 'WINDOW+WALL(AREA)(SQFT)']
+        return pd.DataFrame()
