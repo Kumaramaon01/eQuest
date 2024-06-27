d545483785a8fc66f6ad74b501d8fde6af9c9044
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-27 20:13:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -303,7 +303,7 @@ def main():
         
         if uploaded_file is not None:
             if st.button("Get Schedule INP"):
-                schedule_v01.main(uploaded_file)
+                inp_parserv01.main(uploaded_file)
 
     elif st.session_state.script_choice == "INP Parser":
         st.markdown("""
