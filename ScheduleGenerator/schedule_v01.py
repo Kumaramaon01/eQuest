1a0730acabc25de60dccec78543be6ce4982b178
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-27 20:25:46 +0530

    Update schedule_v01.py

--- ScheduleGenerator/schedule_v01.py
+++ ScheduleGenerator/schedule_v01.py
@@ -4,14 +4,14 @@ from src import schedule
 
 def main(uploaded_file)
     if not os.path.exists(uploaded_file):
-        print("The file does not exist. Please check the path and try again.")
+        st.success("The file does not exist. Please check the path and try again.")
     else:
         try:
             schedules = pd.read_csv(uploaded_file)
             schedule.getScheduleINP(schedules)
         except Exception as e:
-            print(f"An error occurred while reading the CSV file: {e}")
+            st.success(f"An error occurred while reading the CSV file: {e}")
         
 if __name__ == "__main__":
-    uploaded_file = st.file_uploader("Upload your INP file", type=["inp"])
+    uploaded_file = st.file_uploader("Upload CSV or EXCEL file", type=["csv", "xlsx"])
     main(uploaded_file)
