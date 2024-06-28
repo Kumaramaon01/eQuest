0c708a1b50c9becb7bf0ea1be68d285be8170047
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-28 18:33:14 +0530

    Update schedule.py

--- ScheduleGenerator/src/schedule.py
+++ ScheduleGenerator/src/schedule.py
@@ -3,8 +3,29 @@ import pandas as pd
 
 def getScheduleINP(data):
     try:
-        new_file_path = os.path.join(os.path.dirname(__file__), 'new_file.inp')
+        data.columns = data.columns.str.replace(' ', '_')
+        desired_column_name = data.columns[1]
+        file_name = f"{desired_column_name}_Scheduled.inp"
+        new_file_path = os.path.join(os.path.dirname(__file__), file_name)
+
         with open(new_file_path, 'w') as file:
+            # Store index or markers to capture range of values
+            idx1 = 0
+            idx2 = 0
+            idx3 = 0
+        
+            idx1, idx2, idx3 = None, None, None
+            for index, row in data.iterrows():
+                if row[0] == 'Hour':
+                    idx1 = index
+                elif row[0] == 'Day' and idx1 is not None and idx2 is None:
+                    idx2 = index
+                elif row[0] == 'Month' and idx2 is not None and idx3 is None:
+                    idx3 = index + 1  # Adjust index as needed
+                
+                if idx1 is not None and idx2 is not None and idx3 is not None:
+                    break
+
             file.write("INPUT ..\n\n")
             file.write("$ ---------------------------------------------------------\n")
             file.write("$              Abort, Diagnostics\n")
@@ -18,31 +39,35 @@ def getScheduleINP(data):
             # Extracting the 'Hour' row values from 2nd to 25th column
             hour_values = data.loc[data.iloc[:, 0] == 'Hour'].iloc[0, 1:25].tolist()
             formatted_values = ', '.join(map(str, hour_values))
+            type_value = data.iloc[0, 1].upper()
             
             for index, row in data.iterrows():
-                if row[0] != 'Hour':
+                if row[0] == 'Week Schedule' or row[0] == 'Rows can be added to add more weekly schedule': # need to ask this
+                    break
+                if index > idx1:
                     schedule_name = row[0]
                     file.write(f'"{schedule_name}" = DAY-SCHEDULE-PD\n')
-                    file.write("   TYPE             = FRACTION\n")
+                    file.write(f"   TYPE             = {type_value}\n")
                     file.write(f"   VALUES           = ( {formatted_values} )\n")
                     file.write("   ..\n")
                     file.write("")
             
-                    
             # Creating a new section called week schedules after completion of Day Schedule
             file.write("\n$ ---------------------------------------------------------\n")
             file.write("$              Week Schedules\n")
             file.write("$ ---------------------------------------------------------\n\n")
 
             # Extracting the 'Hour' row values from 2nd to 25th column
-            hour_values = data.loc[data.iloc[:, 0] == 'Day'].iloc[0, 1:10].tolist()
+            hour_values = data.loc[data.iloc[:, 0] == 'Day'].iloc[0, 1:11].tolist()
             formatted_values = ', '.join(map(str, hour_values))
             
             for index, row in data.iterrows():
-                if row[0] != 'Hour':
+                if row[0] == 'Annual Schedule' or row[0] == 'Rows can be added to add more weekly schedule': # need to ask this
+                    break
+                if index > idx2:
                     schedule_name = row[0]
                     file.write(f'"{schedule_name}" = WEEK-SCHEDULE-PD\n')
-                    file.write("   TYPE             = FRACTION\n")
+                    file.write(f"   TYPE             = {type_value}\n")
                     file.write(f"   DAY-SCHEDULES    = ( {formatted_values} )\n")
                     file.write("   ..\n")
                     file.write("")
@@ -57,10 +82,12 @@ def getScheduleINP(data):
             formatted_values = ', '.join(map(str, hour_values))
             
             for index, row in data.iterrows():
-                if row[0] != 'Hour':
+                if row[0] == 'nan' or row[0] == 'NaN' or row[0] == 'NAN' or row[0] == 'Rows can be added to add more weekly schedule': # need to ask this
+                    break
+                if index > idx3:
                     schedule_name = row[0]
                     file.write(f'"{schedule_name}" = SCHEDULE-PD\n')
-                    file.write("   TYPE             = FRACTION\n")
+                    file.write(f"   TYPE             = {type_value}\n")
                     file.write(f"   MONTH            = ( {formatted_values} )\n")
                     file.write(f"   DAY              = ( {formatted_values} )\n")
                     file.write(f"   WEEK-SCHEDULES   = ( {formatted_values} )\n")
