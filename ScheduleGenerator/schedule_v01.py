05f75dea4d79c0240f2737371d9d215bd0011ecb
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-28 19:05:11 +0530

    Update schedule_v01.py

--- ScheduleGenerator/schedule_v01.py
+++ ScheduleGenerator/schedule_v01.py
@@ -3,7 +3,7 @@ import pandas as pd
 from src import schedule
 import streamlit as st
 
-def main(uploaded_file)
+def getCSV(uploaded_file)
     if not os.path.exists(uploaded_file):
         st.success("The file does not exist. Please check the path and try again.")
     else:
@@ -14,5 +14,6 @@ def main(uploaded_file)
             st.success(f"An error occurred while reading the CSV file: {e}")
         
 if __name__ == "__main__":
-    uploaded_file = st.file_uploader("Upload CSV or EXCEL file", type=["csv", "xlsx"])
-    main(uploaded_file)
+    # uploaded_file = st.file_uploader("Upload CSV or EXCEL file", type=["csv", "xlsx"])
+    # main(uploaded_file)
+    pass
