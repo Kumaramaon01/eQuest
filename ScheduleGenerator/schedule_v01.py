5c2ad36464ab97bd40e83252f66781ec8ef33487
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-06-28 22:11:13 +0530

    Update schedule_v01.py

--- ScheduleGenerator/schedule_v01.py
+++ ScheduleGenerator/schedule_v01.py
@@ -1,9 +1,9 @@
 import os
 import pandas as pd
-from src import schedule
+from ScheduleGenerator.src import schedule
 import streamlit as st
 
-def getCSV(uploaded_file)
+def getCSV(uploaded_file):
     if not os.path.exists(uploaded_file):
         st.success("The file does not exist. Please check the path and try again.")
     else:
