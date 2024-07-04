15fb6e9c570fd5ed6d80f57a1610b82855722de4
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-07-04 16:02:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -379,7 +379,7 @@ def main():
         # Check if files and reports are selected
         if uploaded_files and reports_input:
             if st.button("Convert to PDF"):
-                st.success("Kindly use exe for now, Will update this soon!!")
+                st.success("!!")
                 # # Clean up each report name
                 # reports = [r.strip() for r in reports_input]
                 
