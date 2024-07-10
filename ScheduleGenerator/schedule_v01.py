825a2b254bc8fd9d5bf40cd59f38f42a4641efb7
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-07-10 17:19:37 +0530

    Update schedule_v01.py

--- ScheduleGenerator/schedule_v01.py
+++ ScheduleGenerator/schedule_v01.py
@@ -93,7 +93,7 @@ def analytics(uploaded_file):
                 return
 
             st.markdown("""
-            <h5 style="color:red;">Daily Schedules of 24 hours</h5>
+            <h4 style="color:red;">Daily Schedules of 24 hours</h4>
             """, unsafe_allow_html=True)
 
             # Your data processing code
