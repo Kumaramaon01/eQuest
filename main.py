08b176086b10c41e7d5ec4f4a593aa48bcf8ed67
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-07-13 19:11:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -227,7 +227,7 @@ def main():
         <style>
         .stButton button {
             height: 30px;
-            width: 162px;
+            width: 163px;
         }
         </style>
     """, unsafe_allow_html=True)
