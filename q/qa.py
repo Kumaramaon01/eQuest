aa765fec594cfed5fddee0e5ccc77bec040f4cd0
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-07-16 16:29:27 +0530

    Update qa.py

--- q/qa.py
+++ q/qa.py
@@ -969,16 +969,18 @@ def getTwoSimFiles(input_simp_path, input_simb_path):
             row3 = data_therm_sum.iloc[0, :-1]  # 3rd row (index 2) 
             row_last = data_therm_sum.iloc[1, :-1]  # Last row 
             # Create pie charts
-            fig1 = px.pie(values=row3.values, names=row3.index, title='Baseline')
-            fig2 = px.pie(values=row_last.values, names=row_last.index, title='Proposed')
-            # Update the traces to show percentages
-            fig1.update_traces(textinfo='percent+label')
-            fig2.update_traces(textinfo='percent+label')
-            col1, col2 = st.columns(2)
-            with col1:
-                st.plotly_chart(fig1)
-            with col2:
-                st.plotly_chart(fig2)
+            if row3.sum() == 0 and row_last.sum() == 0:
+                st.markdown("""<p><strong>Note:</strong> All values are zero. No meaningful visualization can be displayed.</p>""", unsafe_allow_html=True)
+            else:
+                fig1 = px.pie(values=row3.values, names=row3.index, title='Baseline')
+                fig2 = px.pie(values=row_last.values, names=row_last.index, title='Proposed')
+                fig1.update_traces(textinfo='percent+label')
+                fig2.update_traces(textinfo='percent+label')
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.plotly_chart(fig1)
+                with col2:
+                    st.plotly_chart(fig2)
         else:
             st.markdown("""<p><strong>Note:</strong> No data found for THERM & MAX THERM/HR.</p>""", unsafe_allow_html=True)
 
