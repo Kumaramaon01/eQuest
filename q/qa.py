095081f90bdd1cc9dd2af639f59e20b517a4c3ce
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-07-19 11:41:45 +0530

    Update qa.py

--- q/qa.py
+++ q/qa.py
@@ -709,10 +709,10 @@ def getTwoSimFiles(input_simp_path, input_simb_path):
             data_kwh_sum = pd.concat([data_kwh_sum, new_row_last_df]).reset_index(drop=True)
             # Concat other rows called- EFLH Proposed and EFLH Baseline
             # Ensure the data types are numeric
-            data_kwh_sum.iloc[0] = data_kwh_sum.iloc[0].apply(pd.to_numeric, errors='coerce')
-            data_kwh_sum.iloc[1] = data_kwh_sum.iloc[1].apply(pd.to_numeric, errors='coerce')
-            data_kwh_sum.iloc[3] = data_kwh_sum.iloc[3].apply(pd.to_numeric, errors='coerce')
-            data_kwh_sum.iloc[4] = data_kwh_sum.iloc[4].apply(pd.to_numeric, errors='coerce')
+            # data_kwh_sum.iloc[0] = data_kwh_sum.iloc[0].apply(pd.to_numeric, errors='coerce')
+            # data_kwh_sum.iloc[1] = data_kwh_sum.iloc[1].apply(pd.to_numeric, errors='coerce')
+            # data_kwh_sum.iloc[3] = data_kwh_sum.iloc[3].apply(pd.to_numeric, errors='coerce')
+            # data_kwh_sum.iloc[4] = data_kwh_sum.iloc[4].apply(pd.to_numeric, errors='coerce')
             # Perform the division operation with proper index alignment
             # try:
             #     if data_kwh_sum.iloc[0] == 0.0 and data_kwh_sum.iloc[3] == 0.0:
