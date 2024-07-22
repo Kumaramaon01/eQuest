31aa39d3adde2399d30812f71d58955ad2a8749a
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-07-22 16:20:48 +0530

    Update qa.py

--- q/qa.py
+++ q/qa.py
@@ -1367,10 +1367,11 @@ def getTwoSimFiles(input_simp_path, input_simb_path):
             for col in data_kwh_sum1.columns[3:]:
                 if data_kwh_sum1[col].iloc[0] != 0:  # Check to avoid division by zero
                     new_row2[col] = round(data_kwh_sum1[col].iloc[1] / data_kwh_sum1[col].iloc[0], 1)
-                elif data_kwh_sum1[col].iloc[0] == 0 data_kwh_sum1[col].iloc[1] == 0:
+                # elif data_kwh_sum1[col].iloc[0] == 0 data_kwh_sum1[col].iloc[1] == 0:
+                #     new_row2[col] = '0.0'
+                # elif data_kwh_sum1[col].iloc[0] == 0 data_kwh_sum1[col].iloc[1] != 0:
+                else:
                     new_row2[col] = '0.0'
-                elif data_kwh_sum1[col].iloc[0] == 0 data_kwh_sum1[col].iloc[1] != 0:
-                    new_row2[col] = '-'
             
             # Create a DataFrame from the new row
             new_row_df = pd.DataFrame([new_row2])
@@ -1385,10 +1386,11 @@ def getTwoSimFiles(input_simp_path, input_simb_path):
             for col in data_kwh_sum1.columns[3:]:
                 if data_kwh_sum1[col].iloc[3] != 0:
                     new_row3[col] = round(data_kwh_sum1[col].iloc[4] / data_kwh_sum1[col].iloc[3],1)
-                elif data_kwh_sum1[col].iloc[3] == 0 and data_kwh_sum1[col].iloc[4] == 0:
+                # elif data_kwh_sum1[col].iloc[3] == 0 and data_kwh_sum1[col].iloc[4] == 0:
+                #     new_row3[col] = '0.0'
+                # elif data_kwh_sum1[col].iloc[3] == 0 and data_kwh_sum1[col].iloc[4] != 0:
+                else:
                     new_row3[col] = '0.0'
-                elif data_kwh_sum1[col].iloc[3] == 0 and data_kwh_sum1[col].iloc[4] != 0:
-                    new_row3[col] = '-'
             
             # Create a DataFrame from the new row
             new_row_df = pd.DataFrame([new_row3])
