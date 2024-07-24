ca6d64d26efa443250be264ca3902c6665aa3458
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-07-24 16:16:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -272,23 +272,23 @@ def main():
     
     # Second row of buttons
     with col9:
-        if st.button("EXE and Resources", key="button_exe_resources"):
-            st.session_state.script_choice = "exe"
-    with col10:
         if st.button("Schedule Generator", key="button_schedule_generator"): 
             st.session_state.script_choice = "sh"
-    with col11:
+    with col10:
         if st.button("QA / QC", key="button_qa_qc"):
             st.session_state.script_choice = "q"
-    with col12:
-        if st.button("Contact Us", key="button_contact"): #Queries
-            st.session_state.script_choice = "ask"
-    with col13:
+    with col11:
         if st.button("Analytics", key="button_analytics"): #Queries
             st.session_state.script_choice = "sh1"
-    with col14:
+    with col12:
+        if st.button("EXE and Resources", key="button_exe_resources"):
+            st.session_state.script_choice = "exe"
+    with col13:
         if st.button("View References", key="button_references"): #Queries
             st.session_state.script_choice = "reference"
+    with col14:
+        if st.button("Contact Us", key="button_contact"): #Queries
+            st.session_state.script_choice = "ask"
     with col15:
         if st.button("Log In", key="button_login"): #Queries
             st.session_state.script_choice = "login"
