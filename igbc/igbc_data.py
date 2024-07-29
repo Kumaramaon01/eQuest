5198fae881ec74b43f96a730d3cb7c04841d23af
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-07-29 23:28:11 +0530

    Update igbc_data.py

--- igbc/igbc_data.py
+++ igbc/igbc_data.py
@@ -33,6 +33,7 @@ def getINPSimFiles(input_simp_path, input_simb_path):
     csv2 = get_report2.to_csv("report1.csv")
 
     if get_report1 is not None:
+        
         st.write(get_report1)
         # Convert DataFrame to CSV string
         csv1 = get_report1.to_csv(index=False)
