389fbba1a35a0d45753c4f9b0f313419eac43b80
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-07-30 14:50:02 +0530

    Update igbc_parser.py

--- igbc/src/igbc_parser.py
+++ igbc/src/igbc_parser.py
@@ -196,8 +196,8 @@ def get_HVAC_Zone_report(name, name1):
         # Drop rows with any NaN values
         df1 = df1.dropna()
         # Define the name of the new blank column
-        blank_col_name1 = 'Length (M)'
-        blank_col_name2 = 'Width (M)'
+        blank_col_name1 = 'L(M)'
+        blank_col_name2 = 'W(M)'
         blank_col_name3 = 'Openable Type'
 
         df1.insert(loc=df1.columns.get_loc('Carpet Area(Sqft)'), column=blank_col_name1, value=None)
@@ -206,12 +206,12 @@ def get_HVAC_Zone_report(name, name1):
         
         # Add new columns at the end
         df1.insert(loc=len(df1.columns), column='Openable Window Area(Sqft)', value=None)
-        df1.insert(loc=len(df1.columns), column='% of Openable Area to Carpet Area - Required', value=None)
-        df1.insert(loc=len(df1.columns), column='% of Openable Area to Carpet Area - Achieved', value=None)
+        df1.insert(loc=len(df1.columns), column='Openable Area to Carpet Area - Required', value=None)
+        df1.insert(loc=len(df1.columns), column='Openable Area to Carpet Area - Achieved', value=None)
         # 50% value of column name- Window Area (Sqft)
         df1['Window Area (Sqft)'] = pd.to_numeric(df1['Window Area (Sqft)'], errors='coerce')
         # 50% of Area to thic column - '% of Openable Area to Carpet Area - Achieved'
-        df1['% of Openable Area to Carpet Area - Achieved'] = df1['Window Area (Sqft)']/2
+        df1['Openable Area to Carpet Area - Achieved'] = df1['Window Area (Sqft)']/2
 
         ################## Create new dataframe say igbc_vlt which  takes 1st, 5th and 9th column of df1 ####################
         igbc_vlt = df1.iloc[:, [0, 3, 8]]
