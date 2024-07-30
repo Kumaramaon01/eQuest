70b257c220d8ebd0b6d5b8cf9e2da97418dc359f
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-07-30 14:47:29 +0530

    Update igbc_parser.py

--- igbc/src/igbc_parser.py
+++ igbc/src/igbc_parser.py
@@ -228,8 +228,8 @@ def get_HVAC_Zone_report(name, name1):
         igbc_vlt['Glazing Factor Achieved'] = (igbc_vlt['Window Glazing VLT (%)'] * igbc_vlt['Window Area (Sqft)'] * 0.2)/igbc_vlt['Carpet Area(Sqft)']
 
         #create a new column name- "Regularly Occupied Spaces Meet or Exceed the Criteria (Yes/No)" if Glazing Factor Achieved > 1 then YES else NO
-        igbc_vlt['Regularly Occupied Spaces Meet or Exceed the Criteria (Yes/No)'] = None
-        igbc_vlt['Regularly Occupied Spaces Meet or Exceed the Criteria (Yes/No)'] = np.where(igbc_vlt['Glazing Factor Achieved'] > 1, 'YES', 'NO')
+        igbc_vlt['Regularly Equipment (Yes/No)'] = None
+        igbc_vlt['Regularly Equipment (Yes/No)'] = np.where(igbc_vlt['Glazing Factor Achieved'] > 1, 'YES', 'NO')
         igbc_vlt['Min. Glazing Area'] = igbc_vlt['Carpet Area(Sqft)'] /(igbc_vlt['Window Glazing VLT (%)'] * 0.2)
         igbc_vlt['Min. VLT'] = igbc_vlt['Carpet Area(Sqft)'] /(igbc_vlt['Window Area (Sqft)'] * 0.2)
 
