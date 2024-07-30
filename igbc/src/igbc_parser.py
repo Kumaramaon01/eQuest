96f380faf6b6c3b815408e6180a96e771d348524
Ram <138666953+Kumaramaon01@users.noreply.github.com>
2024-07-30 14:57:07 +0530

    Update igbc_parser.py

--- igbc/src/igbc_parser.py
+++ igbc/src/igbc_parser.py
@@ -211,7 +211,7 @@ def get_HVAC_Zone_report(name, name1):
         # 50% value of column name- Window Area (Sqft)
         df1['Window Area (Sqft)'] = pd.to_numeric(df1['Window Area (Sqft)'], errors='coerce')
         # 50% of Area to thic column - '% of Openable Area to Carpet Area - Achieved'
-        df1['Openable Carpet Area - Achieved'] = df1['Window Area (Sqft)']/2
+        df1['Carpet Area - Achieved'] = df1['Window Area (Sqft)']/2
 
         ################## Create new dataframe say igbc_vlt which  takes 1st, 5th and 9th column of df1 ####################
         igbc_vlt = df1.iloc[:, [0, 3, 8]]
